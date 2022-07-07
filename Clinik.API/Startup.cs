7e102e158b05c205a2401d01144a22ddb4ab831b
anderrf <anderson_rocha_farias@outlook.com>
2022-07-06 21:38:42 -0300

    Addition of Swagger UI

--- Clinik.API/Startup.cs
+++ Clinik.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,15 @@ namespace Clinik.API
         {
             services.AddControllers();
             services.AddTransient<IClinicRepository, ClinicRepository>();
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo{
+                    Version = "v1",
+                    Title = "Clinik",
+                    Description = "API for manipulation of clinics and patients' data",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +53,11 @@ namespace Clinik.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>{
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
